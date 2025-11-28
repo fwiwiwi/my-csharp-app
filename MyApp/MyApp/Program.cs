2f6cda283d87294d09159f13cd841406c04df4bc
fwiwiwi <myaumyausdr@gmail.com>
2025-11-28 11:49:00 +0300

    Add files via upload

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет с моего приложнеия! Построена с помощью Github Actions!");
+            Console.WriteLine($"Дата: {DateTime.Now}");
+        }
+    }
+}
